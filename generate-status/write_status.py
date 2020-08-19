4416850eef66c74d829c35ba3e3d7efa8b29cd1d
Jacob See <jasee@redhat.com>
2020-08-19 13:24:07 -0700

    Compose region subdomain based on engagement region (#22)

--- generate-status/write_status.py
+++ generate-status/write_status.py
@@ -24,16 +24,19 @@ with open(f"../../{subject['directory']}/engagement.json", "r") as read_file:
 current_state = subject["anarchy_subject"]["spec"]["vars"]["current_state"]
 desired_state = subject["anarchy_subject"]["spec"]["vars"]["desired_state"]
 
+region = engagement["engagement_region"].lower() if "engagement_region" in engagement else "na"
+region_url = f"{region}-1"
+
 ocp_subsystem["state"] = current_state
 ocp_subsystem["updated"] = str(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc, microsecond=0).isoformat())
 ocp_subsystem["access_urls"] = [
   {
     "title": "Web Console",
-    "url": f"https://console-openshift-console.apps.{engagement['ocp_sub_domain']}.{context['ocp_base_url']}"
+    "url": f"https://console-openshift-console.apps.{engagement['ocp_sub_domain']}.{region_url}.{context['ocp_base_url']}"
   },
   {
     "title": "API",
-    "url": f"https://api.{engagement['ocp_sub_domain']}.{context['ocp_base_url']}:6443"
+    "url": f"https://api.{engagement['ocp_sub_domain']}.{region_url}.{context['ocp_base_url']}:6443"
   }
 ]
 
